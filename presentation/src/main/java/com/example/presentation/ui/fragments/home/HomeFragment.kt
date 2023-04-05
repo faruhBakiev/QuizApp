1c55cdf4528fcf475edcef37233720de87c20fed
faruhBakiev <tanki.onlayn.2017@list.ru>
2023-04-06 00:05:58 +0600

    Multi-Module

---
+++ presentation/src/main/java/com/example/presentation/ui/fragments/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.presentation.ui.fragments.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.presentation.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
